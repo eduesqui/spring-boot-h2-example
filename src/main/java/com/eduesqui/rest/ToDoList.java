b4e6cb4def26409f159f5d50972681f7beb2d2e4
eduesqui <eduesqui87@gmail.com>
2019-07-26 10:43:02 -0500

    [ADD] Logger

--- src/main/java/com/eduesqui/rest/ToDoList.java
+++ src/main/java/com/eduesqui/rest/ToDoList.java
@@ -2,6 +2,8 @@ package com.eduesqui.rest;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import com.eduesqui.model.service.ITaskService;
 @RestController
 @RequestMapping (value="todolist")
 public class ToDoList {
+	
+	private Logger LOG = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private ITaskService taskService;
